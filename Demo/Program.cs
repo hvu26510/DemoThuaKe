0eb5f3501c4646aea37a907f330315efb7fa24dd
hvu26510 <hvu26510@gmail.com>
2023-06-02 18:15:19 +0700

    push

---
+++ Demo/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Timers;
+
+namespace Demo
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList listSV = new ArrayList();
+            addDataDemo(ref listSV);
+          
+            //listSV.Remove(getValue(ref listSV));
+
+            Xuat(listSV);
+
+
+            Console.ReadLine();
+        }
+
+        static void addDataDemo(ref ArrayList list)
+        {
+            SinhVien sv1 = new SinhVien("abc", 10, "abc@email");
+            SinhVien sv2 = new SinhVien("def", 10, "def@email");
+            SinhVien sv3 = new SinhVien("GH", 10, "gh@email");
+            SinhVien sv4 = new SinhVien("YK", 10, "yk@email");
+
+            list.Add(sv1);
+            list.Add(sv2);
+            list.Add(sv3);
+            list.Add(sv4);
+
+        }
+
+        static SinhVien getValue(ref ArrayList list)
+        {
+            foreach (SinhVien item in list)
+            {
+                if(item.getName() == "abc")
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        static void Nhap(ref ArrayList listSV)
+        {
+
+            do
+            {
+                Console.WriteLine("Nhap sinh vien:");
+                SinhVien sv = new SinhVien();
+                sv.Nhap();
+                listSV.Add(sv);
+                Console.WriteLine("Tiep tuc? nhan 'q' De thoat");
+
+                if (Console.ReadLine() == "q")
+                {
+                    break;
+                }
+            } while (true);
+
+        }
+
+        static void Xuat(ArrayList listSV)
+        {
+            foreach (SinhVien i in listSV)
+            {
+                i.Xuat();
+            }
+        }
+    }
+}
---
+++ Demo/SinhVien.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class SinhVien
+    {
+        private string name;
+        private double diem;
+        private string email;
+
+        public string getName()
+        {
+            return this.name;
+        }
+        public string HocLuc()
+        {
+            if(this.diem < 5)
+            {
+                return "Y";
+            }else if(this.diem <6.5)
+            {
+                return "TB";
+            }else if(this.diem < 7.5)
+            {
+                return "K";
+            }
+            else
+            {
+                return "Xuat sac";
+            }
+
+        }
+
+        public void setName(string name)
+        {
+            while (name == "")
+            {
+                Console.WriteLine("Ten rong, yeu cau nhap lai");
+                name = Console.ReadLine();
+            }
+            this.name = name;   
+        }
+
+        public void Nhap()
+        {
+            Console.Write("Ten SV : ");
+            this.setName(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Diem: ");
+            this.diem = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Email: ");
+            this.email = Console.ReadLine();
+        }
+
+        public void Xuat()
+        {
+            Console.WriteLine($"Ten : {this.name}");
+            Console.WriteLine($"Diem : {this.diem}");
+            Console.WriteLine($"email : {this.email}");
+            Console.WriteLine($"Hoc Luc : {this.HocLuc()}");
+        }
+
+        public SinhVien(string name, double diem, string email)
+        {
+            this.name = name;
+            this.diem = diem;
+            this.email = email;
+        }
+
+        public SinhVien()
+        {
+        }
+    }
+}
