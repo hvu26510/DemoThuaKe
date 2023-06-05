b5ffe0424d447c40f529f6db2f34b46fb0cb4ef4
hvu26510 <hvu26510@gmail.com>
2023-06-05 17:38:35 +0700

    push

---
+++ DemoLab5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SinhVienIT svIT = new SinhVienIT("Nguyen van A", "IT", 10, 8, 8);
+
+            SinhVienBiz svBiz = new SinhVienBiz("Nguyen van B", "Biz", 10, 8);
+
+            svIT.xuat();
+            svBiz.xuat();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DemoLab5/SinhVienPoly.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLab5
+{
+    abstract internal class SinhVienPoly
+    {
+        protected string hoTen;
+        protected string nganh;
+        public abstract double getDiem();
+
+        public string getHocLuc()
+        {
+            if (this.getDiem() < 5)
+            {
+                return "Y";
+            }
+            else if (this.getDiem() < 6.5)
+            {
+                return "TB";
+            }
+            else if (this.getDiem() < 7.5)
+            {
+                return "K";
+            }
+            else
+            {
+                return "Xuat sac";
+            }
+
+        }
+
+        public void xuat()
+        {
+            Console.WriteLine("Ho ten: " + this.hoTen);
+            Console.WriteLine("Nganh : " + this.nganh);
+            Console.WriteLine("Diem  : " + this.getDiem());
+            Console.WriteLine("Hoc Luc: " + this.getHocLuc());
+        }
+
+    }
+
+    class SinhVienIT : SinhVienPoly
+    {
+        private double diemJava;
+        private double diemCSS;
+        private double diemHTML;
+        public override double getDiem()
+        {
+            return (this.diemJava*2+ this.diemCSS +this.diemHTML)/4;
+        }
+
+        public SinhVienIT(string hoTen, string nganh,double diemJava, double diemCSS, double diemHTML)
+        {
+            this.hoTen = hoTen;
+            this.nganh = nganh;
+            this.diemJava = diemJava;
+            this.diemCSS = diemCSS;
+            this.diemHTML = diemHTML;
+        }
+    }
+
+    class SinhVienBiz : SinhVienPoly
+    {
+        private double diemMarketing;
+        private double diemSales;
+        public override double getDiem()
+        {
+            return (this.diemMarketing * 2 + this.diemSales) / 3;
+        }
+
+        public SinhVienBiz(string hoTen, string nganh, double diemMarketing, double diemSales)
+        {
+            this.hoTen = hoTen;
+            this.nganh = nganh;
+            this.diemMarketing = diemMarketing;
+            this.diemSales = diemSales;
+        }
+    }
+}
